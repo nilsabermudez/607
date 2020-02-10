bc3fe0a3c7c997c01aaa1a48a929e0d1bac53f0d
nilsabermudez <60415929+nilsabermudez@users.noreply.github.com>
2020-02-09 16:49:34 -0800

    Add files via upload
    MovieSurvey sql script

---
+++ MovieSurvey.sql
@@ -0,0 +1,85 @@
+/*
+  MovieSurvey.sql
+*/
+drop database movies;
+
+CREATE DATABASE movies;
+USE movies;
+/*************************************************/
+/*
+DROP TABLE IF EXISTS Reviewers;
+DROP TABLE IF EXISTS Ratings;
+DROP TABLE IF EXISTS movies;*/
+
+CREATE TABLE IF NOT EXISTS movies (
+  ID INT NOT NULL AUTO_INCREMENT,
+  MovieName VARCHAR(65) NOT NULL,
+  PRIMARY KEY (`ID`));
+
+INSERT INTO movies(MovieName)
+VALUES ("Jumanji: The Next Level"),
+	("Frozen 2"),
+	("Like a Boss"),
+	("Star Wars: The Rise of Skywalker"),
+	("The Gentlemen"),
+	("Bad Boys for Life");
+
+
+CREATE TABLE IF NOT EXISTS Reviewers (
+  ID INT NOT NULL AUTO_INCREMENT,
+  Name VARCHAR(45) NOT NULL,
+  Age INT,
+  Parent CHAR(1) NOT NULL,	
+  PRIMARY KEY (`ID`));
+
+
+INSERT INTO Reviewers (Name, Age, Parent)
+VALUES ("Angel",45,"Y"),
+    ("Maria",32,"Y"),
+    ("Anabell",36,"Y"),
+    ("Jessica",34,"Y"),
+    ("Laura",42,"Y");
+
+CREATE TABLE IF NOT EXISTS Ratings (
+  ReviewerID INT NOT NULL,
+  MovieID INT NOT NULL,
+  Rating INT NOT NULL,
+  PRIMARY KEY (ReviewerID, MovieID),
+  FOREIGN KEY (ReviewerID) REFERENCES Reviewers (ID) ON UPDATE CASCADE,
+  FOREIGN KEY (movieID) REFERENCES movies (ID) ON UPDATE CASCADE
+  );
+
+INSERT INTO Ratings (ReviewerID, MovieID, Rating)
+VALUES(1,1,4),
+	(1,2,3),
+	(1,3,0),
+	(1,4,4),
+	(1,5,0),
+	(1,6,0),
+	(2,1,4),
+	(2,2,3),
+	(2,3,0),
+	(2,4,4),
+	(2,5,0),
+	(2,6,0),
+	(3,1,3),
+	(3,2,3),
+	(3,3,0),
+	(3,4,4),
+	(3,5,0),
+	(3,6,0),
+	(4,1,4),
+	(4,2,3),
+	(4,3,4),
+	(4,4,4),
+	(4,5,0),
+	(4,6,0),
+	(5,1,3),
+	(5,2,3),
+	(5,3,4),
+	(5,4,4),
+	(5,5,0),
+	(5,6,0);
+	
+    
+ 
